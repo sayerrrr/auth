3fb5c4eeb2202149a9fa4c99ab04aee15fcea989
sayerrrr <73977638+sayerrrr@users.noreply.github.com>
2022-04-04 12:37:10 -0700

    bitch ass computer rd 2

--- apps/events/src/scripts/codegen.ts
+++ apps/events/src/scripts/codegen.ts
@@ -1,46 +1,46 @@
-import { codegen } from "@graphql-codegen/core";
-import { getCachedDocumentNodeFromSchema } from "@graphql-codegen/plugin-helpers";
-import * as typescript from "@graphql-codegen/typescript";
-import * as typescriptOperations from "@graphql-codegen/typescript-operations";
-import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
-import { loadDocuments, loadSchema } from "@graphql-tools/load";
-import * as eventBus from "@resource/codegen";
-import { promises } from "fs";
-import path from "path";
-import prettier from "prettier";
+import { codegen } from '@graphql-codegen/core'
+import { getCachedDocumentNodeFromSchema } from '@graphql-codegen/plugin-helpers'
+import * as typescript from '@graphql-codegen/typescript'
+import * as typescriptOperations from '@graphql-codegen/typescript-operations'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadDocuments, loadSchema } from '@graphql-tools/load'
+import * as eventBus from '@resource/codegen'
+import { promises } from 'fs'
+import path from 'path'
+import prettier from 'prettier'
 
 const main = async () => {
   const loadedCDocuments = await loadDocuments(
-    [path.join(process.cwd(), "src/service/graphql/consumer.gql")],
+    [path.join(process.cwd(), 'src/service/graphql/consumer.gql')],
     {
       loaders: [new GraphQLFileLoader()],
-    },
-  );
+    }
+  )
 
   const loadedCSchema = await loadSchema(
-    [path.join(process.cwd(), "src/service/graphql/events.gql")],
+    [path.join(process.cwd(), 'src/service/graphql/events.gql')],
     {
       loaders: [new GraphQLFileLoader()],
-    },
-  );
+    }
+  )
 
   const loadedPSchema = await loadSchema(
-    [path.join(process.cwd(), "src/service/graphql/events.gql")],
+    [path.join(process.cwd(), 'src/service/graphql/events.gql')],
     {
       loaders: [new GraphQLFileLoader()],
-    },
-  );
+    }
+  )
 
   const cConfig = {
     skipTypename: true,
     useTypeImports: true,
     avoidOptionals: true,
-  };
+  }
 
-  const cDocumentNode = getCachedDocumentNodeFromSchema(loadedCSchema);
-  const pDocumentNode = getCachedDocumentNodeFromSchema(loadedPSchema);
+  const cDocumentNode = getCachedDocumentNodeFromSchema(loadedCSchema)
+  const pDocumentNode = getCachedDocumentNodeFromSchema(loadedPSchema)
 
-  const cFilename = path.join(process.cwd(), "../types/event-consumer.ts");
+  const cFilename = path.join(process.cwd(), '../types/event-consumer.ts')
   const consumer = await codegen({
     schema: cDocumentNode,
     documents: loadedCDocuments,
@@ -63,36 +63,36 @@ const main = async () => {
         eventBus: {
           consumer: {
             eventSampler: true,
-            schemaPrintPath: "./src/types/publisher.graphql",
+            schemaPrintPath: './src/types/publisher.graphql',
           },
           scalars: {
-            DateTime: "String",
-            UUID: "String",
-            EmailAddress: "String",
+            DateTime: 'String',
+            UUID: 'String',
+            EmailAddress: 'String',
           },
         },
       },
     ],
-  });
+  })
 
   await promises.writeFile(
     cFilename,
     prettier.format(consumer, {
       ...(await prettier.resolveConfig(process.cwd())),
-      parser: "typescript",
+      parser: 'typescript',
     }),
     {
-      encoding: "utf-8",
-    },
-  );
+      encoding: 'utf-8',
+    }
+  )
 
   const pConfig = {
     enumsAsTypes: true,
     skipTypename: true,
     useTypeImports: true,
     avoidOptionals: true,
-  };
-  const pFilename = path.join(process.cwd(), "../types/event-producer.ts");
+  }
+  const pFilename = path.join(process.cwd(), '../types/event-producer.ts')
   const producer = await codegen({
     schema: pDocumentNode,
     documents: loadedCDocuments,
@@ -115,28 +115,28 @@ const main = async () => {
         eventBus: {
           publisher: true,
           scalars: {
-            DateTime: "String",
-            UUID: "String",
-            EmailAddress: "String",
+            DateTime: 'String',
+            UUID: 'String',
+            EmailAddress: 'String',
           },
         },
       },
     ],
-  });
+  })
 
   await promises.writeFile(
     pFilename,
     prettier.format(producer, {
       ...(await prettier.resolveConfig(process.cwd())),
-      parser: "typescript",
+      parser: 'typescript',
     }),
     {
-      encoding: "utf-8",
-    },
-  );
-};
+      encoding: 'utf-8',
+    }
+  )
+}
 
 main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+  console.error(err)
+  process.exit(1)
+})
